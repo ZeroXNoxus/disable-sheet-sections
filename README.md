a2789e2e982ad2b6e42f7dd9fb3e917347c04d2b
NikoKuck <nikolai.kucksdorf@live.de>
2023-11-07 12:22:16 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Latest Version](https://img.shields.io/github/v/release/ZeroXNoxus/disable-sheet-sections?display_name=tag&sort=semver&label=Latest%20Version)](https://github.com/ZeroXNoxus/dark-mode-5e/releases/latest)
+![Foundry Version](https://img.shields.io/endpoint?url=https%3A%2F%2Ffoundryshields.com%2Fversion%3Fstyle%3Dflat%26url%3Dhttps%3A%2F%2Fgithub.com%2FZeroXNoxus%2Fdisable-sheet-sections%2Freleases%2Fdownload%2Flatest%2Fmodule.json)
+
+![Latest Downloads](https://img.shields.io/github/downloads/ZeroXNoxus/disable-sheet-sections/latest/total?color=blue&label=latest%20downloads)
+![Total Downloads](https://img.shields.io/github/downloads/ZeroXNoxus/disable-sheet-sections/total?color=blue&label=total%20downloads)
+
+# DnD 5e Disable Sheet Sections
+## Package Description ##
+A simple module bringing customizablity to the DnD 5th edition character sheet. Disable Inspiration? No more Exhaustion? Done!
+## Installation ##
+Simply download the module, either via the the 'Install Module' feature in the 'Setup' screen, or by downloading the latest release from the github repository.
+## Preview ##
+## Usage ##
+# New Feature #
+# Supported Modules #
---
+++ script/disable-sheet-sections.js
@@ -0,0 +1,84 @@
+function initialize(){
+    toggleSections();
+}
+
+function toggleSections(){
+    const inspiration = game.settings.get('disable-sheet-sections', 'inspiration');
+    const exhaustion  = game.settings.get('disable-sheet-sections', 'exhaustion');
+    const ressources  = game.settings.get('disable-sheet-sections', 'ressources');
+    const traits      = game.settings.get('disable-sheet-sections', 'traits');
+    switch(true){
+        case inspiration:
+            $('body').addClass('disable-inspiration');
+            break;
+        case exhaustion:
+            $('body').addClass('disable-exhaustion');
+            break;
+        case ressources:
+            $('body').addClass('disable-ressources');
+            break;
+        case traits:
+            $('body').addClass('disable-traits');
+            break;
+    }
+    switch(false){
+        case inspiration:
+            $('body').removeClass('disable-inspiration');
+            break;
+        case exhaustion:
+            $('body').removeClass('disable-exhaustion');
+            break;
+        case ressources:
+            $('body').removeClass('disable-ressources');
+            break;
+        case traits:
+            $('body').removeClass('disable-traits');
+            break;
+    }
+}
+
+function generateSheetSectionSettings(){ 
+    game.settings.register('disable-sheet-sections', 'inspiration', {
+        name: 'Disable Inspiration',
+        hint: "When selected the 'Inspiration' field on character sheets will be hidden.",
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { toggleSections(); }
+    });
+    game.settings.register('disable-sheet-sections', 'exhaustion', {
+        name: 'Disable Exhaustion',
+        hint: "When selected the 'Exhaustion' field on character sheets will be hidden.",
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { toggleSections(); }
+    });
+    game.settings.register('disable-sheet-sections', 'ressources', {
+        name: 'Disable Exhaustion',
+        hint: "When selected the 'Ressource' fields on character sheets will be hidden.",
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { toggleSections(); }
+    });
+    game.settings.register('disable-sheet-sections', 'traits', {
+        name: 'Disable Special Traits',
+        hint: "When selected the 'Special Traits' setting on character sheets will be hidden.",
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => { toggleSections(); }
+    });
+}
+Hooks.on('init', () => {
+    generateSheetSectionSettings();
+});
+
+Hooks.on('renderActorSheet', () => {
+    initialize();
+});
